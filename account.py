9920e00c34f9c2942671cecb9fae544510669544
Parker Gornick <parkergornick@gmail.com>
2022-11-17 15:31:29 -0600

    Final part of lab

--- account.py
+++ account.py
@@ -1,16 +1,30 @@
 class Account:
-    def __init__(self, name: str):
-        self.__account_name = Account(name)
+    def __init__(self, name: str) -> None:
+        """
+        Initialize the variable for account name and account balance.
+        :param name: Account name is equal to what is put in for account. Account balance is set to 0.
+        """
+        self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount: float):
+    def deposit(self, amount: float) -> bool:
+        """
+        This function increases by the amount given.
+        :param amount: Amount added to the account.
+        :return: True if not negative or zero then will be false.
+        """
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount: float):
+    def withdraw(self, amount: float) -> bool:
+        """
+        This function decreases the amount given.
+        :param amount: Amount subtracted to the account.
+        :return: True if amount is not negative, zero or more than the account balance. Then will return false.
+        """
         if amount <= 0:
             return False
         elif amount > self.__account_balance:
@@ -19,10 +33,18 @@ class Account:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Get the account balance.
+        :return: Account balance.
+        """
         return self.__account_balance
 
-    def get_name(self):
-        return self.__account_balance
+    def get_name(self) -> str:
+        """
+        Get the account name.
+        :return: Account name.
+        """
+        return self.__account_name
 
 
---
+++ test_account.py
@@ -0,0 +1,50 @@
+from pytest import *
+from account import *
+
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('Parker')
+        self.a2 = Account('Ashley')
+
+    def teardown_method(self):
+        del self.a1
+        del self.a2
+
+    def test_init_(self):
+        assert self.a1.get_name() == 'Parker'
+        assert self.a1.get_balance() == 0
+        assert self.a2.get_name() == 'Ashley'
+        assert self.a2.get_balance() == 0
+
+    def test_deposit(self):
+        assert self.a1.deposit(-1) == False
+        assert self.a1.get_balance() == 0
+        assert self.a1.deposit(1) == True
+        assert self.a1.get_balance() == 1
+        assert self.a1.deposit(0) == False
+        assert self.a1.get_balance() == 1
+
+        assert self.a2.deposit(-1) == False
+        assert self.a2.get_balance() == 0
+        assert self.a2.deposit(1) == True
+        assert self.a2.get_balance() == 1
+        assert self.a2.deposit(0) == False
+        assert self.a2.get_balance() == 1
+
+    def test_withdraw(self):
+        self.a1.deposit(2)
+        assert self.a1.withdraw(-1) == False
+        assert self.a1.get_balance() == 2
+        assert self.a1.withdraw(0) == False
+        assert self.a1.get_balance() == 2
+        assert self.a1.withdraw(1) == True
+        assert self.a1.get_balance() == 1
+
+        self.a2.deposit(2)
+        assert self.a2.withdraw(-1) == False
+        assert self.a2.get_balance() == 2
+        assert self.a2.withdraw(0) == False
+        assert self.a2.get_balance() == 2
+        assert self.a2.withdraw(1) == True
+        assert self.a2.get_balance() == 1
